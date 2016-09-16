3c0b05e7b51c9c5a81f094975f0e102d7f209599
ken_miller <ken_miller521@yahoo.com>
2016-09-16 11:08:10 -0700

    Implemented health, timer, and partial joystick controls

---
+++ Assets/Scripts/UI/HUDManager.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+/*This script implements everything regarding to test HUD. Controls
+The hurt effect effect, virtual joystick, and enemy wave countdown timer. 
+Parts of this script could be moved to scripts
+if needed to consolidate*/
+public class HUDManager : MonoBehaviour {
+	public GameObject hurtEffectObject;
+	public GameObject timerTextBox;
+	public float flashSpeed;
+	public float startingWaveTimer;
+	public Slider slider;
+	private float _waveTimer;
+	private Text _waveTimerText;
+	private GameObject _player;
+	private TEMP_PLAYER_SCRIPT _playerCont;
+	private Image _hurtEffectImg;
+	private Color _tempColor = new Color(1,1,1,1);
+	private bool _flashingCoroutineActive = false;
+	// Use this for initialization
+	void Start () {
+		_player = GameObject.Find ("Player");
+		_playerCont = _player.GetComponent<TEMP_PLAYER_SCRIPT> ();
+		_hurtEffectImg = hurtEffectObject.GetComponent<Image> ();
+		_waveTimerText = timerTextBox.GetComponent<Text> ();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		_waveTimer -= Time.deltaTime;
+		_waveTimerText.text = _waveTimer.ToString("F1");
+		if (_waveTimer <= 0f) 
+		{
+			Debug.Log ("Enemy wave incoming.");
+			_waveTimer = startingWaveTimer;
+		}
+
+		if (_playerCont.playerHealth > 10f) {
+			if (_flashingCoroutineActive) 
+			{
+				_flashingCoroutineActive = false;
+				StopCoroutine ("FlashingHealth");
+			}
+			_tempColor.a = Mathf.Abs (_playerCont.playerHealth - 100f) * 0.01f;
+			//Debug.Log (_tempColor);
+			_hurtEffectImg.color = _tempColor;
+		} 
+		else 
+		{
+			if (_flashingCoroutineActive == false) 
+			{
+				Debug.Log ("STARTING COROUTINE");
+				_flashingCoroutineActive = true;
+				StartCoroutine ("FlashingHealth");
+			}
+		}
+		/*
+		if (_playerHealth <= 10f) {
+		} else if (_playerHealth > 10f && _playerHealth <= 50f) {
+		}
+		else if(_playerHealth > 50f && < 100f)
+		{
+		}
+		if (_playerHealth == 100f) 
+		{
+			_tempColor.a = 0f;
+			_hurtEffectImg.color = _tempColor;
+		}*/
+	
+	}
+	public void sliderBarChangeHealth()
+	{
+		_playerCont.playerHealth = slider.value * 100f;
+	}
+
+	IEnumerator FlashingHealth()
+	{
+		bool _fadeIn = true;
+		_tempColor.a = _hurtEffectImg.color.a;
+		while (_playerCont.playerHealth <= 10f) 
+		{
+			if (_fadeIn) 
+			{
+				if (_hurtEffectImg.color.a <= 1f) 
+				{
+					_tempColor.a += Time.deltaTime * flashSpeed;
+					_hurtEffectImg.color = _tempColor;
+				} else
+					_fadeIn = false;
+			} 
+			else 
+			{
+				if (_hurtEffectImg.color.a >= 0f) 
+				{
+					_tempColor.a -= Time.deltaTime * flashSpeed;
+					_hurtEffectImg.color = _tempColor;
+				} 
+				else
+					_fadeIn = true;
+			}
+			Debug.Log ("HERE");
+			yield return new WaitForSeconds (0.1f);
+		}
+		yield return null;
+	}
+}
---
+++ Assets/Scripts/UI/TEMP_PLAYER_SCRIPT.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*Strictly a temporary script for player movment. Copied from Player.cs
+Purpose of this script is to avoid merge conflicts since other people
+could be working on the same Player.cs script*/
+public class TEMP_PLAYER_SCRIPT : MonoBehaviour {
+	public float playerHealth = 100f;
+	const float PLAYER_SPEED = 0.1f;
+	public VirtualJoystick joystick;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			playerHealth += Time.deltaTime*13f;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			playerHealth -= Time.deltaTime*13f;
+		}
+				
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(new Vector3(0,0,5));
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(new Vector3(0, 0, -5));
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += (transform.right * PLAYER_SPEED);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position += (-transform.right * PLAYER_SPEED);
+		}
+	}
+}
