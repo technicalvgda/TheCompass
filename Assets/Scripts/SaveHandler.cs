cdf93c9fba5a352fbd0b16545ca8dc2019651716
kdo70 <kdo70@yahoo.com>
2016-09-16 15:19:52 -0700

    Added SaveScene and SaveHandler

---
+++ Assets/Scripts/SaveHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SaveHandler : MonoBehaviour {
+	public Button save;
+	public Button back;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onSave() {
+		Debug.Log ("Save");
+	}
+}
