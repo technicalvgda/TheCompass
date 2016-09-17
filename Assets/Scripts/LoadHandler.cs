3a23302a8f1dab4097b874f3794f167406cc3dad
kdo70 <kdo70@yahoo.com>
2016-09-16 21:37:04 -0700

    Created LoadScene and Handler. Added Prompt for Saving.

---
+++ Assets/Scripts/LoadHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class LoadHandler : MonoBehaviour {
+	public Text slotText;
+	void Start()
+	{
+		slotText.text = PlayerPrefs.GetString ("onLevel");
+	}
+		
+	public void onLoad() 
+	{
+		Debug.Log ("Loading: " + PlayerPrefs.GetString("onLevel"));
+	}
+
+	public void onBack()
+	{
+		//No menu to go back to yet
+	}
+		
+
+
+}
--- Assets/Scripts/SaveHandler.cs
+++ Assets/Scripts/SaveHandler.cs
@@ -2,19 +2,42 @@
 using System.Collections;
 using UnityEngine.UI;
 public class SaveHandler : MonoBehaviour {
-	public Button save;
-	public Button back;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public Text slotText;
+	public GameObject onScreenPrompt;
+	void Start()
+	{
+		slotText.text = PlayerPrefs.GetString ("onLevel");
+		onScreenPrompt.SetActive (false);
 	}
 
-	public void onSave() {
-		Debug.Log ("Save");
+	public void onSave() 
+	{
+		onScreenPrompt.SetActive (true);
 	}
+
+	public void onLoad() 
+	{
+		Debug.Log ("Loading: " + PlayerPrefs.GetString("onLevel"));
+	}
+
+	public void onBack()
+	{
+		//No menu to go back to yet
+	}
+
+	public void promptYes()
+	{
+		//Random number generator to test the saving feature
+		PlayerPrefs.SetString("onLevel", "Level " + Random.Range(1,100));
+		slotText.text = PlayerPrefs.GetString ("onLevel");
+		Debug.Log ("Saving: " + PlayerPrefs.GetString("onLevel"));
+		onScreenPrompt.SetActive (false);
+	}
+
+	public void promptNo() 
+	{
+		onScreenPrompt.SetActive (false);
+	}
+
+
 }
