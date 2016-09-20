67d5e73205270a70c12c0fb93fe85c40273e7402
Stanley Ung <stanleyung167@gmail.com>
2016-09-20 03:00:02 -0700

    Player Stats
    A script to encapsulate all the stats of the player. For now we only
    have health, but we may add other things late like power or defenses.
    Only have gain health and take damages functions so far.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+    // Health Variables
+    private float playerHealth;
+    private float shipHealth;
+    bool alive= true;
+
+	// Use this for initialization
+	void Start () {
+        playerHealth = 1000f; // Set health to 1000 for now. This can be changed later
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void gainHealth(float health) {
+        playerHealth += health;
+    }
+
+    void takeDamage(float damage) {
+        playerHealth -= damage;    
+    }
+}
