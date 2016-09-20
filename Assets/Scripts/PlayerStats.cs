3e5ea013a67695e79d36701be60ed3b0c425c319
Stanley Ung <stanleyung167@gmail.com>
2016-09-20 02:55:05 -0700

    Revert "Revert "Player Stats""
    This reverts commit 3a9defebabad29735ef5b44214a7129ec9ac6044.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+    // health variables
+    // two Entities or one
+    // Should we have ship and a human player as two seperate entities?
+    public float playerHealth;
+    public float shipHealth;
+    bool alive = true;
+
+    // Use this for initialization
+    void Start () {
+        playerHealth = 1000f; // Start at 1000 health. This can be changed later depending on how we want to use it.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void gainHealth(float health) {
+        playerHealth += health;
+    }
+
+    void takeDamage(float damage) {
+        playerHealth -= damage;
+    }
+    
+
+}
