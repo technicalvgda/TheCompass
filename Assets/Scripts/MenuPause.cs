eafde81d04b87a5dd4f629eacef06db1b04d7be3
Michael Trinidad <mikestheking95@yahoo.com>
2016-09-19 20:59:27 -0700

    Implemented Pause Menu
    Created canvas that will pop up when ESC is pressed. Includes 'PAUSED'
    text and three buttons: 'Resume', 'Options', 'Quit'.
    
    Resume sets the canvas inactive again.
    
    Options (right now) pushes a message to log and will later implement
    options menu.
    
    Exit Game closes the application.

---
+++ Assets/Scripts/MenuPause.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPause : MonoBehaviour {
+
+    public GameObject PauseUI;
+
+    //boolean value determining pause state
+    private bool isPaused = false;
+
+    //hides pause menu at the start and duration of the game
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    //runs once every frame
+    void Update()
+    {
+        //if ESC button is pressed, change the pause state
+        if (Input.GetButtonDown("Pause"))
+        {
+            isPaused = !isPaused;
+        }
+
+        //if paused, bring up pause menu && stop game time
+        if (isPaused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        //if not paused, deactivate pause menu && continue game time
+        if (!isPaused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    //Resume button method, changes pause state to original
+    public void Resume()
+    {
+        isPaused = false;
+    }
+
+    //Options button method, pushes log message to assure button has been pressed. Will implement later
+    public void Options()
+    {
+        Debug.Log("Options menu has been accessed");
+    }
+
+    //Quit button method, closes game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
+
