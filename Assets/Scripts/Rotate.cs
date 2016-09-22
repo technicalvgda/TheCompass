c2d3b84cb0b398bccfce40b9294128c0dc6f4ad5
Darkchicken <joshshucker@hotmail.com>
2016-09-22 12:32:30 -0700

    Added Rotate Script
    Copied in Rone's rotate script to avoid conflicts

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+    // Use this for initialization
+    void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateLeft();
+    }
+
+    //Physics
+    void RotateLeft()
+    {
+        transform.Rotate(Vector3.forward);
+    }
+}
