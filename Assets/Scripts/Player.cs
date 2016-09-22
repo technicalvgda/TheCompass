777084a37dfd1861efeab56892778e30067ab7db
Darkchicken <joshshucker@hotmail.com>
2016-09-22 11:42:42 -0700

    Created a new scene for entire branch
    Added scripts to moveable objects to set size
    Removed tags
    Added line renderer for tractor beam

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/* 
+*   Script to handle all player controls
+*   and stat changes
+*
+*/
 public class Player : MonoBehaviour {
 
+    //PLAYER COMPONENTS
+    private Rigidbody2D rb2d;
+
+    // STAT VARIABLES
     const float PLAYER_SPEED = 40.0f;
     const float BRAKE_SPEED = 20.0f;
     const float ROTATION_SPEED = 5.0f;
-    private Rigidbody2D rb2d;
 
-	// Use this for initialization
-	void Start () 
+    public float playerStartingHealth;//< the amount of health the player begins with
+    private float playerHealth;//< the player's current health
+    bool alive = true; //<bool for whether player is alive
+
+    // Use this for initialization
+    void Start () 
 	{
+        //get rigidbody component of player object
 		rb2d = GetComponent<Rigidbody2D> ();
+        //set player health to starting health
+        playerHealth = playerStartingHealth;
 	}
 
 	void Update ()
     {
-		/*
+        //Function to handle player movement
+        ControlPlayer();
+
+    }
+
+    private void ControlPlayer()
+    {
+        /*
 		Controls player movement
 			A and D rotates player
 			W and S accelerates and decelerates player
@@ -24,10 +45,19 @@ public class Player : MonoBehaviour {
         float rotation = Input.GetAxis("Horizontal");
         float acceleration = Input.GetAxis("Vertical");
 
-        transform.Rotate(new Vector3(0,0,-ROTATION_SPEED * rotation));
+        transform.Rotate(new Vector3(0, 0, -ROTATION_SPEED * rotation));
 
-        rb2d.AddForce (transform.up * PLAYER_SPEED * acceleration);
+        rb2d.AddForce(transform.up * PLAYER_SPEED * acceleration);
+    }
 
 
+    void gainHealth(float health)
+    {
+        playerHealth += health;
+    }
+
+    void takeDamage(float damage)
+    {
+        playerHealth -= damage;
     }
 }
