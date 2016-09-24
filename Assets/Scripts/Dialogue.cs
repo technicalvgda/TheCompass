4c2d2ed18701c5eb31f4426d4ab681fbb0335711
kdo70 <kdo70@yahoo.com>
2016-09-24 12:15:30 -0700

    Added DialogueScene and Script
    -Added a way to change the speed and color of text

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour 
+{
+	public Text text;
+	public float textSpeed = .1f;
+	public Color textColor;
+	void Start () 
+	{
+		text = GetComponent<Text> ();
+		text.color = new Color(textColor.r, textColor.g, textColor.b);
+		StartCoroutine (AnimateText("This is a textThis is a textThis is a textThis is a textThis is a textThis is a textThis is a textThis is a textThis is a textThis is a text"));
+	}
+	
+	IEnumerator AnimateText(string textInput) 
+	{
+		int i = 0;
+		text.text = "";
+		while(i < textInput.Length)
+		{
+			text.text += textInput [i++];
+			yield return new WaitForSeconds (textSpeed);
+		}
+	}
+}
