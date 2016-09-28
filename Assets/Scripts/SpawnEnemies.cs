de729fa280caac2ef80e24a85124f019be089b0b
AdVictoriam420 <davidguan567@gmail.com>
2016-09-27 21:32:10 -0700

    EnemySpawner
    Created a prefab that can be place anyway, but will spawn the enemy at a
    specific point, depending on the number in the script.

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemies : MonoBehaviour {
+public Transform Enemy;
+//This script will spawn enemies
+	void Start ()
+	{
+//InvokeRepeating will spawn the enemy in seconds. Change the number if you want to increase the spawn time or decrease
+		InvokeRepeating ("SpawnEnemy", 3F, 3F);
+	}
+	public void SpawnEnemy()
+	{
+//Instantiate spawn the enemy
+//Random.Range determine the location of the spawn. I set it to spawn in one spot
+		Instantiate(Enemy, new Vector3(Random.Range (-2, -2), Random.Range (3, 3), 0), Quaternion.identity);
+	}
+}
