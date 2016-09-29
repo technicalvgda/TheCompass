6d68f211d5419f3100f73209f9a6141b2e176da4
Darkchicken <joshshucker@hotmail.com>
2016-09-29 12:33:46 -0700

    Improvements to Enemy Spawn script

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -1,18 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class SpawnEnemies : MonoBehaviour {
-public Transform Enemy;
 //This script will spawn enemies
+public class SpawnEnemies : MonoBehaviour
+{
+    //the enemy to spawn at the point
+    public GameObject Enemy;
+
+    //how long to wait before spawning
+    public float spawnTime = 3.0f;
+
+    //checks if the spawner should be testing for death
+    private bool enemySpawned = false;
+    //stores the currently spawned enemy, if the enemy is inactive, spawn another
+    private GameObject currentEnemy = null; 
+
+
 	void Start ()
 	{
-//InvokeRepeating will spawn the enemy in seconds. Change the number if you want to increase the spawn time or decrease
-		InvokeRepeating ("SpawnEnemy", 3F, 3F);
+        //Invoke spawns enemy after 
+		Invoke("SpawnEnemy", spawnTime);
 	}
+
+    void Update()
+    {
+        //if the spawner is testing for death and the enemy is dead
+        if(enemySpawned == true && currentEnemy == null)
+        {
+            //Invoke spawns enemy after 
+            Invoke("SpawnEnemy", spawnTime);
+        }
+    }
+
 	public void SpawnEnemy()
 	{
-//Instantiate spawn the enemy
-//Random.Range determine the location of the spawn. I set it to spawn in one spot
-		Instantiate(Enemy, new Vector3(Random.Range (-2, -2), Random.Range (3, 3), 0), Quaternion.identity);
+        if(currentEnemy == null)
+        {
+            //Instantiate spawn the enemy, store as currentEnemy
+            currentEnemy = Instantiate(Enemy, transform.position, Quaternion.identity) as GameObject;
+            enemySpawned = true;
+        }
+        
 	}
+
+    
 }
