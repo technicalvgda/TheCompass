018273541ff88d2399b3a7ce3a56229fd79b72f2
Timothy Touch <chaosbounder@gmail.com>
2016-10-02 23:28:26 -0700

    Implement Barrier
    -The barrier simply pushes the player back to where it first entered the
    barrier

---
+++ Assets/Barrier.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrier : MonoBehaviour {
+
+	private bool isInBarrier = false;
+	private Collider2D player;
+	private Vector2 playerPos = new Vector2 (0, 0);
+
+	void Update()
+	{
+		if (isInBarrier && player != null) {
+			player.SendMessage ("resetUTurnTime");
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "PlayerPlaceholder") {
+			//Debug.Log (other.name + " is outta there");
+			player = other;
+			playerPos = player.transform.position;
+			player.SendMessage ("setPlayerExitPos", playerPos);
+			Debug.Log (playerPos);
+			isInBarrier = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "PlayerPlaceholder") {
+			//Debug.Log (other.name + " is back in there babieeee");
+			player = other;
+			isInBarrier = false;
+		}
+		
+	}
+}
