5e682b1dba3fc5236add701cbdc95853f113ae00
Darkchicken <joshshucker@hotmail.com>
2016-10-06 17:11:13 -0700

    Parallax Handler Fully Functional

--- Assets/Scripts/ParallaxHandlerScript.cs
+++ Assets/Scripts/ParallaxHandlerScript.cs
@@ -6,10 +6,6 @@ public class ParallaxHandlerScript : MonoBehaviour {
     //1 is closest layer, 3 is furthest layer (background panel)
     public GameObject layer1, layer2, layer3;
 
-    //Transform[] layer1Children;
-    //Transform[] layer2Children;
-    //Transform[] layer3Children;
-
     //unit to measure size of map by (5 is value used by editor grid for 1 cell)
     public float unit = 5.0f;
 
@@ -20,61 +16,36 @@ public class ParallaxHandlerScript : MonoBehaviour {
     Vector3 centerPos;
 
     Camera mainCamera;
-    GameObject player;
-    Vector2 playerPos;
+    float cameraHalfWidth;
+    float cameraHalfHeight;
 
     //variables for layer math
     //can add position and half width or height to get edge pos of layer
+    private float width1, width2, width3;
+    private float height1, height2, height3;
+
     private float halfWidth1, halfWidth2, halfWidth3;
     private float halfHeight1, halfHeight2, halfHeight3;
-
-    float maxX1, maxX2, maxX3;
-    float maxY1, maxY2, maxY3;
-
 
     Vector3 bottomLeft;
     Vector3 bottomRight;
     Vector3 topLeft;
     Vector3 topRight;
-    /*
-    //draw lines to show map size
-    void OnDrawGizmos()
-    {
-      
-            Gizmos.DrawLine(new Vector3(-mapSizeX, y, 0.0f),
-                            new Vector3(mapSizeX, y, 0.0f));
-      
-            Gizmos.DrawLine(new Vector3(x, -mapSizeY, 0.0f),
-                            new Vector3(x, mapSizeY, 0.0f));
-        
-
-    }
-    */
+   
     // Use this for initialization
     void Start ()
     {
-        //Get corner positions
-        bottomLeft = new Vector3(-levelSizeX / 2 * unit, -levelSizeY / 2 * unit, 0);
-        bottomRight = new Vector3(levelSizeX / 2 * unit, -levelSizeY / 2 * unit, 0);
-        topLeft = new Vector3(-levelSizeX / 2 * unit, levelSizeY / 2 * unit, 0);
-        topRight = new Vector3(levelSizeX / 2 * unit, levelSizeY / 2 * unit, 0);
-        //draw lines for map borders
-        Debug.DrawLine(bottomLeft, bottomRight,Color.green);//< bottom line
-        Debug.DrawLine(topLeft, topRight, Color.green);//< top line
-        Debug.DrawLine(bottomLeft, topLeft, Color.green);//< left line
-        Debug.DrawLine(bottomRight, topRight, Color.green);//< right line
+       
 
 
         //assign camera and player variable
         mainCamera = Camera.main;
-        player = GameObject.FindGameObjectWithTag("Player");
         centerPos = mainCamera.transform.position;
+        cameraHalfHeight = Camera.main.orthographicSize;
+        cameraHalfWidth = Camera.main.orthographicSize * Screen.width / Screen.height ;
+        
 
-        //layer1Children = new Transform[layer1.transform.childCount];
-        //layer2Children = new Transform[layer2.transform.childCount];
-        //layer3Children = new Transform[layer3.transform.childCount];
-
-       //set sorting layers of all children to each layer
+        //set sorting layers of all children to each layer
         foreach (Transform child in layer1.transform)
         {child.GetComponent<SpriteRenderer>().sortingLayerName = "Parallax1";}
 
@@ -85,12 +56,19 @@ public class ParallaxHandlerScript : MonoBehaviour {
         { child.GetComponent<SpriteRenderer>().sortingLayerName = "Background"; }
 
         //set layer math variables
-        halfWidth1 = layer1.GetComponent<RectTransform>().rect.width / 2;
-        halfWidth2 = layer2.GetComponent<RectTransform>().rect.width / 2;
-        halfWidth3 = layer3.GetComponent<RectTransform>().rect.width / 2;
-        halfHeight1 = layer1.GetComponent<RectTransform>().rect.height / 2;
-        halfHeight2 = layer2.GetComponent<RectTransform>().rect.height / 2;
-        halfHeight3 = layer3.GetComponent<RectTransform>().rect.height / 2;
+        width1 = layer1.GetComponent<RectTransform>().rect.width;
+        width2 = layer2.GetComponent<RectTransform>().rect.width;
+        width3 = layer3.GetComponent<RectTransform>().rect.width;
+        height1 = layer1.GetComponent<RectTransform>().rect.height;
+        height2 = layer2.GetComponent<RectTransform>().rect.height;
+        height3 = layer3.GetComponent<RectTransform>().rect.height;
+
+        halfWidth1 = width1 / 2;
+        halfWidth2 = width2 / 2;
+        halfWidth3 = width3 / 2;
+        halfHeight1 = height1 / 2;
+        halfHeight2 = height2 / 2;
+        halfHeight3 = height3 / 2;
 
         
     }
@@ -98,44 +76,60 @@ public class ParallaxHandlerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-
-        //draw lines for map borders
-        Debug.DrawLine(bottomLeft, bottomRight, Color.green);//< bottom line
-        Debug.DrawLine(topLeft, topRight, Color.green);//< top line
-        Debug.DrawLine(bottomLeft, topLeft, Color.green);//< left line
-        Debug.DrawLine(bottomRight, topRight, Color.green);//< right line
-
-        playerPos = player.transform.position;
+        //draw box for level border
+        DrawDebugBox(Color.green, levelSizeX * unit, levelSizeY*unit, true, Vector3.zero);
+       
 
         //handle shifting of all 3 layers
-        HandleLayerMovement(layer1.transform, halfWidth1, halfHeight1, maxX1, maxY1);
-        HandleLayerMovement(layer2.transform, halfWidth2, halfHeight2, maxX2, maxY2);
-        HandleLayerMovement(layer3.transform, halfWidth3, halfHeight3, maxX3, maxY3);
+        HandleLayerMovement(layer1.transform, halfWidth1, halfHeight1);
+        HandleLayerMovement(layer2.transform, halfWidth2, halfHeight2);
+        HandleLayerMovement(layer3.transform, halfWidth3, halfHeight3);
        
     }
 
-    void HandleLayerMovement(Transform currentLayer, float halfWidth, float halfHeight,
-                             float maxX, float maxY)
+    void HandleLayerMovement(Transform currentLayer, float halfWidth, float halfHeight)
     {
-        //width of layer
-        float width = currentLayer.GetComponent<RectTransform>().rect.width;
-        //height of layer
-        float height = currentLayer.GetComponent<RectTransform>().rect.height;
 
-        //float shiftRatioX = halfWidth / (levelSizeX * unit);  //0.2f;
-        //float shiftRatioY = halfHeight / (levelSizeY * unit);//0.2f;
-
-        float shiftRatioX = 1 - ((levelSizeX * unit / 2) - mainCamera.transform.position.x) / (levelSizeX * unit / 2) - centerPos.x;
-        float shiftRatioY = 1 - ((levelSizeY * unit / 2) - mainCamera.transform.position.y) / (levelSizeY * unit / 2) - centerPos.y;
+        float shiftRatioX = 1  - (((levelSizeX * unit / 2) - mainCamera.transform.position.x) / (levelSizeX * unit / 2) - centerPos.x);
+        float shiftRatioY = 1 - (((levelSizeY * unit / 2) - mainCamera.transform.position.y) / (levelSizeY * unit / 2) - centerPos.y);
         //needs to shift by a certain degree so that it starts centered
         //and shifts just enough to reach its edge at the edge of the map
 
-        float layerXPos = mainCamera.transform.position.x - (width / 2 * shiftRatioX);
-        float layerYPos = mainCamera.transform.position.y - (height / 2 * shiftRatioY);
+        //move the layer to the position of camera with offset
+        float layerXPos = mainCamera.transform.position.x - ((halfWidth- cameraHalfWidth)* shiftRatioX);
+        float layerYPos = mainCamera.transform.position.y - ((halfHeight -cameraHalfHeight)* shiftRatioY);
 
         currentLayer.position = new Vector2(layerXPos,layerYPos);
 
-        
+        //draw box for layer 1
+        DrawDebugBox(Color.yellow, width1, height1, false, currentLayer.position);
+        //draw box for layer 2
+        DrawDebugBox(Color.blue, width2, height2, false, currentLayer.position);
+        //draw box for layer 3
+        DrawDebugBox(Color.red, width3, height3, false, currentLayer.position);
 
+    }
+
+    void DrawDebugBox(Color color, float xSize, float ySize, bool levelMap, Vector3 shift)
+    {
+        //Get corner positions
+        bottomLeft = new Vector3(-xSize / 2, -ySize / 2, 0);
+        bottomRight = new Vector3(xSize / 2, -ySize / 2, 0);
+        topLeft = new Vector3(-xSize / 2, ySize / 2, 0);
+        topRight = new Vector3(xSize / 2, ySize / 2 , 0);
+
+        if(!levelMap)
+        {
+            bottomLeft += shift;
+            bottomRight += shift;
+            topLeft += shift;
+            topRight += shift;
+        }
+
+        //draw lines for map borders
+        Debug.DrawLine(bottomLeft, bottomRight, color);//< bottom line
+        Debug.DrawLine(topLeft, topRight, color);//< top line
+        Debug.DrawLine(bottomLeft, topLeft, color);//< left line
+        Debug.DrawLine(bottomRight, topRight, color);//< right line
     }
 }
