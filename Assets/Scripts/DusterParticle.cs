94d63db76f2de3c13375804e99e6358735e17ae4
jedisongoku <beratbekce@gmail.com>
2016-10-07 10:52:21 -0700

    Duster Enemy Particle Added
    - Drone now leaves a trail behind

---
+++ Assets/Scripts/DusterParticle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DusterParticle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnParticleCollision(GameObject test)
+    {
+        Debug.Log("HITT");
+    }
+}
