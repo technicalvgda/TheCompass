28c3a60997b305d2dc930e6b6f98437fd637279d
Ganondorfa <hlnpabst@gmail.com>
2016-10-08 13:05:29 -0700

    Steroid Splitter
    -Made a "Steroid" splitter that breaks into 8 pieces that travel faster
    than other splitter shards
    -Made original splitter break into 4 pieces instead of 3 (for scale)

---
+++ Assets/Scripts/SplitterShard2.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplitterShard2 : MonoBehaviour 
+{
+	//PUBLIC VARIABLES
+	public bool drift = true;
+	public float objectSize = 1;
+
+	private float driftSpeed = 1000;
+	private Rigidbody2D rb2d;
+
+
+	void Start () 
+	{
+		//get rigidbody component 
+		rb2d = GetComponent<Rigidbody2D>();
+		if (drift)
+		{
+			float x = Random.Range(-1f, 1f);
+			float y = Random.Range(-1f, 1f);
+			Vector2 direction = new Vector2(x, y).normalized;
+			rb2d.AddForce(direction * driftSpeed);
+		}
+	}
+
+	void Update ()
+	{
+
+	}
+}
--- Assets/Scripts/SteroidSplitter.cs
+++ Assets/Scripts/SteroidSplitter.cs
@@ -6,7 +6,7 @@ public class SteroidSplitter : MonoBehaviour
 	//PUBLIC VARIABLES
 	public bool drift;
 	public float objectSize = 5;
-	public GameObject splitter, splitterShard;
+	public GameObject splitter, splitterShard2;
 	public float splitterX, splitterY;
 
 	private float driftSpeed = 100;
@@ -41,9 +41,9 @@ public class SteroidSplitter : MonoBehaviour
 	void OnCollisionEnter2D(Collision2D col)
 	{
 		Destroy (splitter);
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < 8; i++)
 		{
-			Instantiate (splitterShard, new Vector3(splitterX, splitterY, 0), Quaternion.identity);
+			Instantiate (splitterShard2, new Vector3(splitterX, splitterY, 0), Quaternion.identity);
 		}
 	}
 
