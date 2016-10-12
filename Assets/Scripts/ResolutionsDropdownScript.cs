00d4c6096bda0db250402920ea2c805c084fcf8b
Michael Trinidad <mikestheking95@yahoo.com>
2016-10-12 15:05:28 -0700

    Implemented Resolutions and Fullscreen
    fixed last commit's problems

--- Assets/Scripts/ResolutionsDropdownScript.cs
+++ Assets/Scripts/ResolutionsDropdownScript.cs
@@ -9,14 +9,9 @@ public class ResolutionsDropdownScript : MonoBehaviour
 
     public Toggle fullscreenToggle;
 
-    void Start()
+    void Update()
     {
-        //resolutionDropdown.onValueChanged.AddListener(delegate
-        //{
-            //resolutionDropdownValueChangedHandler(resolutionDropdown);
-
-        //}
-        //);
+        resolutionDropdownValueChangedHandler(resolutionDropdown);
     }
 
     public void resolutionDropdownValueChangedHandler(Dropdown target)
@@ -24,70 +19,70 @@ public class ResolutionsDropdownScript : MonoBehaviour
         switch (target.value)
         {
             case 0:
-                Screen.SetResolution(1920, 1080, fullscreenToggle);
+                Screen.SetResolution(1920, 1080, fullscreenToggle.isOn);
                 break;
             case 1:
-                Screen.SetResolution(1680, 1050, fullscreenToggle);
+                Screen.SetResolution(1680, 1050, fullscreenToggle.isOn);
                 break;
             case 2:
-                Screen.SetResolution(1600, 900, fullscreenToggle);
+                Screen.SetResolution(1600, 900, fullscreenToggle.isOn);
                 break;
             case 3:
-                Screen.SetResolution(1440, 900, fullscreenToggle);
+                Screen.SetResolution(1440, 900, fullscreenToggle.isOn);
                 break;
             case 4:
-                Screen.SetResolution(1400, 1050, fullscreenToggle);
+                Screen.SetResolution(1400, 1050, fullscreenToggle.isOn);
                 break;
             case 5:
-                Screen.SetResolution(1366, 768, fullscreenToggle);
+                Screen.SetResolution(1366, 768, fullscreenToggle.isOn);
                 break;
             case 6:
-                Screen.SetResolution(1360, 768, fullscreenToggle);
+                Screen.SetResolution(1360, 768, fullscreenToggle.isOn);
                 break;
             case 7:
-                Screen.SetResolution(1280, 1024, fullscreenToggle);
+                Screen.SetResolution(1280, 1024, fullscreenToggle.isOn);
                 break;
             case 8:
-                Screen.SetResolution(1280, 960, fullscreenToggle);
+                Screen.SetResolution(1280, 960, fullscreenToggle.isOn);
                 break;
             case 9:
-                Screen.SetResolution(1280, 800, fullscreenToggle);
+                Screen.SetResolution(1280, 800, fullscreenToggle.isOn);
                 break;
             case 10:
-                Screen.SetResolution(1280, 768, fullscreenToggle);
+                Screen.SetResolution(1280, 768, fullscreenToggle.isOn);
                 break;
             case 11:
-                Screen.SetResolution(1280, 720, fullscreenToggle);
+                Screen.SetResolution(1280, 720, fullscreenToggle.isOn);
                 break;
             case 12:
-                Screen.SetResolution(1280, 600, fullscreenToggle);
+                Screen.SetResolution(1280, 600, fullscreenToggle.isOn);
                 break;
             case 13:
-                Screen.SetResolution(1152, 864, fullscreenToggle);
+                Screen.SetResolution(1152, 864, fullscreenToggle.isOn);
                 break;
             case 14:
-                Screen.SetResolution(1024, 768, fullscreenToggle);
+                Screen.SetResolution(1024, 768, fullscreenToggle.isOn);
                 break;
             case 15:
-                Screen.SetResolution(800, 600, fullscreenToggle);
+                Screen.SetResolution(800, 600, fullscreenToggle.isOn);
                 break;
             case 16:
-                Screen.SetResolution(640, 480, fullscreenToggle);
+                Screen.SetResolution(640, 480, fullscreenToggle.isOn);
                 break;
             case 17:
-                Screen.SetResolution(640, 400, fullscreenToggle);
+                Screen.SetResolution(640, 400, fullscreenToggle.isOn);
                 break;
             case 18:
-                Screen.SetResolution(512, 384, fullscreenToggle);
+                Screen.SetResolution(512, 384, fullscreenToggle.isOn);
                 break;
             case 19:
-                Screen.SetResolution(400, 300, fullscreenToggle);
+                Screen.SetResolution(400, 300, fullscreenToggle.isOn);
                 break;
             case 20:
-                Screen.SetResolution(320, 240, fullscreenToggle);
+                Screen.SetResolution(320, 240, fullscreenToggle.isOn);
                 break;
             case 21:
-                Screen.SetResolution(320, 200, fullscreenToggle);
+                Screen.SetResolution(320, 200, fullscreenToggle.isOn);
                 break;
 
         }
