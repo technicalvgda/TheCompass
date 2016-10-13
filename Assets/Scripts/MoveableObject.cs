a61e299044fa7f63c6c58ca9b5f406c28ff7c750
Darkchicken <joshshucker@hotmail.com>
2016-10-13 12:19:35 -0700

    Added Laser emitter graphics

--- Assets/Scripts/MoveableObject.cs
+++ Assets/Scripts/MoveableObject.cs
@@ -69,49 +69,64 @@ public class MoveableObject : MonoBehaviour
 
 			if (playerObject != null) {	
 
+                /*
 				Debug.Log ("----------------------------------------------");
 
 				Debug.Log ("Current object tag: " + this.gameObject.tag);
 				Debug.Log ("Collided object tag: " + col.gameObject.tag);
+                */
 
 				_asteroidVelocity = rb2d.velocity.magnitude;
 
+                /*
 				Debug.Log ("Apos: " + col.transform.position);
 				Debug.Log ("PPos: " + transform.position);
-
+                */
 
 				asteroidDirection = (col.transform.position - transform.position).normalized;
 
+                /*
 				Debug.Log ("Asteroid Direction: " + rb2d.velocity.normalized);
 				Debug.Log ("Asteroid Direction from Player: " + (col.transform.position - transform.position));
 				Debug.Log ("Asteroid Direction from Player Normalized: " + asteroidDirection);
 				Debug.Log ("Asteroid rigidbody velocity: " + rb2d.velocity);
 				Debug.Log ("Asteroid rigidbody velocity Normalized: " + rb2d.velocity.normalized);
+                */
 
 				_directionStatus = Vector2.Dot (asteroidDirection, rb2d.velocity.normalized);
 
+                /*
 				Debug.Log ("Dot Product: " + _directionStatus);
+                */
 
 				if (_asteroidVelocity > _VELOCITYMINIMUM) 
 				{
 					_objectVelocityMet = true;
+                    /*
 					Debug.Log ("Object Velocity met?" + _objectVelocityMet);
+                    */
 				}
 
 				if (_asteroidVelocity > _asteroidMinimum && _directionStatus > 0f) {
 					_playerDamaged = true;
 					_asteroidDamageForce = Mathf.Round (_asteroidVelocity * _ASTEROIDFORCECONSTANT); 
 
+                    /*
 					print ("Player current health= " + playerObject.getHealth ());
+                    */
 
 					if (_playerDamaged) {	
 						playerObject.takeDamage (_asteroidDamageForce);
 
+                        /*
 						print ("Player damage= " + _asteroidDamageForce);
 						print ("Player health after damage= " + playerObject.getHealth ());
+                        */
 					}
+                    /*
 					print (_playerDamaged);
 					print ("Velocity= " + _asteroidVelocity);
+                    */
 				}
 			}
 		}
