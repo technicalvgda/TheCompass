59dd7967c0cb090db928702eaad7f272e85494a3
Cherisle <michaeltran95@live.com>
2016-10-14 02:08:52 -0700

    Updated Collision detection for Debris Borders
    - collisions no longer work by trigger exit
    - collisions detected with raycast
    - updated test scene

--- Assets/Scripts/DebrisObjectChildHorz.cs
+++ Assets/Scripts/DebrisObjectChildHorz.cs
@@ -3,15 +3,62 @@ using System.Collections;
 
 public class DebrisObjectChildHorz : MonoBehaviour
 {
+	private float maxRayDistance;
+	private float boxCollSizeX,boxCollSizeY; // NOTE: vals should be same if square
+	private float obXLoc, obYLoc;
+	private Vector2 topBoundCorner;
+	Ray2D rayBound;
+	RaycastHit2D hit;
 
+	void FixedUpdate()
+	{
+		if (transform.gameObject.name == "DebObLeft")
+		{
+			topBoundCorner = new Vector2 (obXLoc - boxCollSizeX / 2, obYLoc + boxCollSizeY / 2);
+		} 
+		else if (transform.gameObject.name == "DebObRight")
+		{
+			topBoundCorner = new Vector2 (obXLoc + boxCollSizeX / 2, obYLoc + boxCollSizeY / 2);
+		} 
+		else
+		{
+			topBoundCorner = new Vector2 (obXLoc, obYLoc); // dummy value, default
+		}
+		rayBound = new Ray2D (topBoundCorner, Vector2.down);
+		Debug.DrawLine (topBoundCorner, topBoundCorner + Vector2.down * maxRayDistance);
+
+	}
 	// Use this for initialization
-	void Start () {
-
+	void Start ()
+	{
+		maxRayDistance = GetComponent<BoxCollider2D>().size.y;
+		boxCollSizeX = GetComponent<BoxCollider2D>().size.x;
+		boxCollSizeY = GetComponent<BoxCollider2D>().size.y;
+		obXLoc = transform.position.x;
+		obYLoc = transform.position.y;
 	}
 
 	// Update is called once per frame
-	void Update () {
-
+	void Update ()
+	{
+		if (transform.gameObject.name == "DebObLeft")
+		{
+			hit = Physics2D.Raycast (rayBound.origin, rayBound.direction);
+			if (hit != null && hit.collider != null && hit.collider.GetComponent<MoveableObject>() != null)
+			{
+				Debug.Log ("Detected collision with Ray2D");
+				transform.parent.GetComponent<DebrisBorderHorz>().collWithObLeft(hit.collider);
+			}
+		}
+		if (transform.gameObject.name == "DebObRight")
+		{	
+			hit = Physics2D.Raycast (rayBound.origin, rayBound.direction);
+			if (hit != null && hit.collider != null && hit.collider.GetComponent<MoveableObject>() != null)
+			{
+				Debug.Log ("Detected collision with Ray2D");
+				transform.parent.GetComponent<DebrisBorderHorz>().collWithObRight(hit.collider);
+			}
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D coll)
@@ -21,17 +68,6 @@ public class DebrisObjectChildHorz : MonoBehaviour
 
 	void OnTriggerExit2D(Collider2D coll)
 	{
-		if(coll.GetComponent<MoveableObject>() != null) // MoveableObject script does exist
-		{
-			if (transform.gameObject.name == "DebObLeft")
-			{
-				transform.parent.GetComponent<DebrisBorderHorz>().collWithObLeft(coll);
-			}
-
-			if (transform.gameObject.name == "DebObRight")
-			{
-				transform.parent.GetComponent<DebrisBorderHorz>().collWithObRight(coll);
-			}
-		}
+		// do nothing
 	}
 }
--- Assets/Scripts/DebrisObjectChildVert.cs
+++ Assets/Scripts/DebrisObjectChildVert.cs
@@ -3,15 +3,61 @@ using System.Collections;
 
 public class DebrisObjectChildVert : MonoBehaviour
 {
+	private float maxRayDistance;
+	private float boxCollSizeX,boxCollSizeY; // NOTE: vals should be same if square
+	private float obXLoc, obYLoc;
+	private Vector2 leftBoundCorner;
+	Ray2D rayBound;
+	RaycastHit2D hit;
+
+	void FixedUpdate()
+	{
+		if (transform.gameObject.name == "DebObUpper")
+		{
+			leftBoundCorner = new Vector2 (obXLoc - boxCollSizeX / 2, obYLoc + boxCollSizeY / 2);
+		} 
+		else if (transform.gameObject.name == "DebObLower")
+		{
+			leftBoundCorner = new Vector2 (obXLoc - boxCollSizeX / 2, obYLoc - boxCollSizeY / 2);
+		} 
+		else
+		{
+			leftBoundCorner = new Vector2 (obXLoc, obYLoc); // dummy value, default
+		}
+		rayBound = new Ray2D (leftBoundCorner, Vector2.right);
+		Debug.DrawLine (leftBoundCorner, leftBoundCorner + Vector2.right * maxRayDistance);
+
+	}
 	void Start ()
 	{
-
+		maxRayDistance = GetComponent<BoxCollider2D>().size.x;
+		boxCollSizeX = GetComponent<BoxCollider2D>().size.x;
+		boxCollSizeY = GetComponent<BoxCollider2D>().size.y;
+		obXLoc = transform.position.x;
+		obYLoc = transform.position.y;
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-
+		if (transform.gameObject.name == "DebObUpper")
+		{
+			hit = Physics2D.Raycast (rayBound.origin, rayBound.direction);
+			if (hit != null && hit.collider != null && hit.collider.GetComponent<MoveableObject>() != null)
+			{
+				Debug.Log ("Detected collision with Ray2D");
+				transform.parent.GetComponent<DebrisBorderVert>().collWithObUpper(hit.collider);
+			}
+		}
+		if (transform.gameObject.name == "DebObLower")
+		{	
+			hit = Physics2D.Raycast (rayBound.origin, rayBound.direction);
+			if (hit != null && hit.collider != null && hit.collider.GetComponent<MoveableObject>() != null)
+			{
+				Debug.Log ("Detected collision with Ray2D");
+				transform.parent.GetComponent<DebrisBorderVert>().collWithObLower(hit.collider);
+			}
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D coll)
@@ -21,17 +67,6 @@ public class DebrisObjectChildVert : MonoBehaviour
 
 	void OnTriggerExit2D(Collider2D coll)
 	{
-		if(coll.GetComponent<MoveableObject>() != null) // MoveableObject script does exist
-		{
-			if (transform.gameObject.name == "DebObUpper")
-			{
-				transform.parent.GetComponent<DebrisBorderVert>().collWithObUpper(coll);
-			}
-
-			if (transform.gameObject.name == "DebObLower")
-			{
-				transform.parent.GetComponent<DebrisBorderVert>().collWithObLower(coll);
-			}
-		}
+		//do nothing
 	}
 }
