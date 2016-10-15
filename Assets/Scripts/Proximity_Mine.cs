cd345f6b97d2e5de983a91a3dce907dc40ade7cd
Joel Lee <lee.joel395@gmail.com>
2016-10-14 17:34:12 -0700

    VolatileWreck_Start
    Started work on the exploding enemies.

---
+++ Assets/Scripts/Proximity_Mine.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Proximity_Mine : MonoBehaviour {
+	public float damage, pushForce, explRadius;
+
+	private CircleCollider2D _col;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		Debug.Log("Entered");
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(this.transform.position, explRadius);
+		if(col.gameObject.tag == "Player"){
+			Debug.Log("In Range");
+			foreach(Collider2D hit in colliders){
+				float dx = hit.transform.position.x - transform.position.x;
+				float dy = hit.transform.position.y - transform.position.y;
+				Vector2 vect = new Vector2(dx, dy);
+				hit.attachedRigidbody.AddForce(vect.normalized * pushForce, ForceMode2D.Impulse);
+			}
+		}
+	}
+}
