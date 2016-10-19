33672ed99fb71772783fd4bbb3329dc3533c6603
j95alvarez <juanalvarez391@yahoo.com>
2016-10-19 13:52:57 -0700

    Level2
    Added a background images and player and enemy objects.

---
+++ Assets/exit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exit : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player") {
+            Debug.Log("End Level");
+            Time.timeScale = 0;
+        }
+    }
+}
