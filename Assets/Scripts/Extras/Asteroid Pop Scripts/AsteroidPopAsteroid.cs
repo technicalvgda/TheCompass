474f089b8a2c79cd6aadbbe24f6eee3905c28633
Ken Miller <ken.miller521@gmail.com>
2016-10-19 22:07:00 -0700

    Even more work on Asteroid Pop

--- Assets/Scripts/Extras/Asteroid Pop Scripts/AsteroidPopAsteroid.cs
+++ Assets/Scripts/Extras/Asteroid Pop Scripts/AsteroidPopAsteroid.cs
@@ -3,17 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class AsteroidPopAsteroid : MonoBehaviour {
+	public bool isShootable;
 	public string color;
 	public List<GameObject> adjacentAsteroids;
 	public Collider2D[] hitColliders;
-	public Vector3 _center;
 	// Use this for initialization
 	void Start () {		
 		Debug.Log (color);
+		hitColliders = Physics2D.OverlapCircleAll (transform.position, 2.5f);
+		collectSameColoredAdjacentAsteroids ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		hitColliders = Physics2D.OverlapCircleAll (transform.position, 2.5f);
+	void Update () 
+	{
+		if (isShootable) 
+		{
+			
+		}
+		
+	}
+	void collectSameColoredAdjacentAsteroids()
+	{
+		for (int i = 0; i < hitColliders.Length; i++) 
+		{
+			if (hitColliders [i].GetComponent<AsteroidPopAsteroid> ().color == color)
+				adjacentAsteroids.Add (hitColliders [i].gameObject);
+		}
 	}
 }
