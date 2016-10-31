0063d7eaba69c85030396c4eccaa8eab03527595
KingdomCross <alexchheng1998@yahoo.com>
2016-10-30 23:32:39 -0700

    Scripts semi-complete
    Able to get almost all requirement done. simply change the desire
    objects tag to a "Wall" to be a bouncy obsticle to the player.
    Hasn't found a pushback effect to the player, need more research to find
    it or help.

---
+++ Assets/Scripts/WallKnockback.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallKnockback : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float thrust;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Wall")
+        {
+            transform.position = new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f);
+            rb2d.AddForce()
+        }
+    }
+}
