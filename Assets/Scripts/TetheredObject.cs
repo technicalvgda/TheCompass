cf2b17e0aedb756b7495ea793dbaa7b6f0966bd7
Rone <rone_caballero@yahoo.com>
2016-11-02 11:13:35 -0700

    Made Tethered object health mechanic
    In the Tether Scene, there is a tethered health mechanic. All it does is
    manage the object's health. Current setting is set to 5 HP. -1 for every
    hit from an object labeled as "enemy". When HP reaches 0, tethered
    object destroyed

--- Assets/Scripts/TetheredObject.cs
+++ Assets/Scripts/TetheredObject.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class TetheredObject : MonoBehaviour {
 
+    public int TetheredHealth = 5; //tethered object's health. Gets hit five times and health goes to zero. 
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,17 @@ public class TetheredObject : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        {
+            TetheredHealth -= 1; // tethered health loses 1 health point 
+            if (TetheredHealth <= 0) // if tethered health reaches zero or below
+            {
+                Destroy(col.gameObject); //the collided object goes bye bye
+            }
+        }
+    }
 }
