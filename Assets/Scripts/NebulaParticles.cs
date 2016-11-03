899d10dbe3ae03ccba1fc47b6e7f082bbe770882
Darkchicken <joshshucker@hotmail.com>
2016-11-03 11:58:19 -0700

    Fixed ion constructs

--- Assets/Scripts/NebulaParticles.cs
+++ Assets/Scripts/NebulaParticles.cs
@@ -8,12 +8,16 @@ public class NebulaParticles : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        //set emission rate to reflect size of construct
         float emissionRate = emissionMultiplier * transform.lossyScale.magnitude;
         particles = GetComponent<ParticleSystem>();
         ParticleSystem.EmissionModule em = particles.emission;//.rate.Equals();
         ParticleSystem.MinMaxCurve rate = new ParticleSystem.MinMaxCurve();
         rate.constantMax = emissionRate;
         em.rate = rate;
+
+        //set particle size to reflect size of construct
+        particles.startSize = particles.startSize + (emissionRate/20);
     } 
 	
 }
