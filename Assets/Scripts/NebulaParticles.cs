decf9946d467af17a155833a1b2979e9bf874bff
Darkchicken <joshshucker@hotmail.com>
2016-11-03 14:05:29 -0700

    Fixed nebula particles

--- Assets/Scripts/NebulaParticles.cs
+++ Assets/Scripts/NebulaParticles.cs
@@ -5,6 +5,13 @@ public class NebulaParticles : MonoBehaviour
 {
     ParticleSystem particles;
     int emissionMultiplier = 10;
+
+    //gizmo code
+    Vector3 lowerRight;
+    Vector3 upperRight;
+    Vector3 lowerLeft;
+    Vector3 upperLeft;
+
     // Use this for initialization
     void Start()
     {
@@ -18,6 +25,22 @@ public class NebulaParticles : MonoBehaviour
 
         //set particle size to reflect size of construct
         particles.startSize = particles.startSize + (emissionRate/20);
-    } 
-	
+    }
+
+    void OnDrawGizmos()
+    {
+        float xOffset = GetComponent<Collider2D>().bounds.size.x/2;
+        float yOffset = GetComponent<Collider2D>().bounds.size.y /2;
+        lowerLeft = new Vector3(transform.position.x - xOffset,transform.position.y-yOffset,0);
+        upperLeft = new Vector3(transform.position.x - xOffset, transform.position.y + yOffset, 0);
+        lowerRight = new Vector3(transform.position.x + xOffset, transform.position.y - yOffset, 0);
+        upperRight = new Vector3(transform.position.x + xOffset, transform.position.y + yOffset, 0);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(lowerLeft, lowerRight);
+        Gizmos.DrawLine(lowerLeft, upperLeft);
+        Gizmos.DrawLine(upperLeft, upperRight);
+        Gizmos.DrawLine(upperRight, lowerRight);
+
+    }
+
 }
