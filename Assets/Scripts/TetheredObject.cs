31744a1d178cdbe4264c3dad5c1619990baef7db
Cherisle <michaeltran95@live.com>
2016-11-03 21:28:05 -0700

    Tether Line Implementation
    + Updated TetheredObjectTest scene
    + Added Tether Line when player collects TetherObject
    + line is white, 0.1f wide

--- Assets/Scripts/TetheredObject.cs
+++ Assets/Scripts/TetheredObject.cs
@@ -1,28 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TetheredObject : MonoBehaviour {
-
-    public int TetheredHealth = 5; //tethered object's health. Gets hit five times and health goes to zero. 
+public class TetheredObject : MonoBehaviour
+{
+	private bool tetherOn;
+	float xPos,yPos,zPos;
+	private Color c1 = Color.white;
+	private Vector3 playerPosition;
+	private LineRenderer lineRen;
+    public int TetheredHealth = 3; //tethered object's health. Gets hit three times and health goes to zero. 
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		tetherOn = false;
+		lineRen = gameObject.AddComponent<LineRenderer>();
+		lineRen.material = new Material(Shader.Find("Particles/Additive"));
+		lineRen.SetWidth(.1f , .1f);
+		lineRen.SetColors (c1, c1);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	    
+	void Update ()
+	{
+		if (tetherOn == true)
+		{
+			xPos = GameObject.Find("PlayerPlaceholder").transform.position.x;
+			yPos = GameObject.Find("PlayerPlaceholder").transform.position.y;
+			zPos = GameObject.Find("PlayerPlaceholder").transform.position.z;
+			playerPosition = new Vector3 (xPos, yPos, zPos);
+			lineRen.SetPosition (0, playerPosition);
+			lineRen.SetPosition (1, transform.position);
+		}
 	}
 
-    void OnCollisionEnter2D(Collision2D col)
+    void OnCollisionEnter2D(Collision2D coll)
     {
-        {
-            TetheredHealth -= 1; // tethered health loses 1 health point 
-            if (TetheredHealth <= 0) // if tethered health reaches zero or below
-            {
-                Destroy(col.gameObject); //the collided object goes bye bye
-            }
-        }
+		if (coll.gameObject.name == "PlayerPlaceholder")
+		{
+			tetherOn = true;
+			playerPosition = new Vector3(coll.transform.position.x,coll.transform.position.y,coll.transform.position.z);
+		} 
+		else
+		{
+			TetheredHealth -= 1; // tethered health loses 1 health point 
+			if (TetheredHealth <= 0) // if tethered health reaches zero or below
+			{
+				tetherOn = false;
+				Destroy (coll.gameObject); //the collided object goes bye bye
+			}
+		}
     }
 }
