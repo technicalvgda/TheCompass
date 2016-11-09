f14196eacb3dddd42543735eadf17681194a4951
TCanet <tcanet94@gmail.com>
2016-11-08 19:25:29 -0800

    Triple Shot + Bullet Script Polish
    Attack Script has been added which seperates code from the drone vision
    script. Triple Shot is in the attack Script and just needs to be ticked
    in the Editor. Attack Script now needs to be added to prefabs which
    require it. Bullets now instantiate in front of parent GameObject
    Bugs: Drone Vision Turns 180 Constantly, sometimes enters a patroling
    pattern (not related to attack script)

---
+++ Assets/Attack.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+	GameObject bullet, bullet1, player;
+	public GameObject bulletPrefab;
+	DroneVision dVScript;
+	public bool tripleShot, currentlyShooting;
+
+	// Use this for initialization
+	void Start () 
+	{
+		dVScript = gameObject.GetComponent<DroneVision> ();
+		currentlyShooting = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (dVScript.playerFound == true) 
+		{
+			if (currentlyShooting == false)
+				StartCoroutine (shoot ());
+		}
+	}
+
+	IEnumerator shoot()
+	{
+		currentlyShooting = true;
+		Instantiate(bulletPrefab, transform.position+(transform.right*2), transform.rotation);
+
+		if (tripleShot) 
+		{
+			bullet = (GameObject)Instantiate(bulletPrefab, transform.position+(transform.right*2), transform.rotation);
+			bullet.transform.Rotate (0, 0, 45);
+
+			bullet1 = (GameObject)Instantiate(bulletPrefab, transform.position+(transform.right*2), transform.rotation);
+			bullet1.transform.Rotate (0, 0, -45);
+		}
+
+		//bullet.GetComponent<Bullet> ().parent = gameObject;
+		yield return new WaitForSeconds(1);
+
+		if (dVScript.playerFound == true)
+		{
+			StartCoroutine (shoot ());
+		}else
+			currentlyShooting = false;
+	}
+}
