8059b7e211838bb19b52673d14c685fd0f3f5398
Darkchicken <joshshucker@hotmail.com>
2016-11-17 09:59:30 -0800

    Added Health bar
    Enemy Collision now spawns a health bar for enemy
    Added black hole graphics

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class EnemyCollision : MonoBehaviour 
 {
@@ -9,38 +10,22 @@ public class EnemyCollision : MonoBehaviour
     public float InitialHealth = 20f;
 	private float Health;
 
-    //TODO add instantiation and handler for health bars
-    float barDisplay = 0;
-    Vector2 size  = new Vector2(60,20);
-    Texture2D progressBarEmpty;
-    Texture2D progressBarFull;
-
-    void OnGUI()
-    {
-        progressBarEmpty = Resources.Load("Texture2") as Texture2D;
-        progressBarFull = Resources.Load("Texture2B") as Texture2D;
-        // draw the background:
-        GUI.BeginGroup(new Rect(transform.position.x, transform.position.y, size.x, size.y));
-        GUI.Box(new Rect(0, 0, size.x, size.y), progressBarEmpty);
-
-        // draw the filled-in part:
-        GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
-        GUI.Box(new Rect(0, 0, size.x, size.y), progressBarFull);
-        GUI.EndGroup();
-
-        GUI.EndGroup();
-    }
+    //Canvas to contain health bar
+    Canvas healthBarCanvas;
+    // Actual health bar slider
+    Slider healthBar;
 
     void Start()
     {
+        InitializeHealthBar();
         //set starting health
         Health = InitialHealth;
-        barDisplay = Health;
+        UpdateHealthBar();
     }
     public void TakeDamage(float damage)
     {
         Health -= damage;
-        barDisplay = Health;
+        UpdateHealthBar();
         //check if the enemy is out of health
         CheckHealth();
     }
@@ -55,6 +40,22 @@ public class EnemyCollision : MonoBehaviour
             Player.increaseKillCount();
             Instantiate(Items, transform.position, Quaternion.identity);
         }
+    }
+
+    //returns a value between 0 and 1 based on health remaining (1 is full, 0 is empty)
+    void UpdateHealthBar()
+    {
+        healthBar.value = Health / InitialHealth;
+    }
+
+    void InitializeHealthBar()
+    {
+        //Instantiate the health bar canvas at the location of the enemy
+        healthBarCanvas = Instantiate(Resources.Load("HealthBarCanvas"), transform.position, transform.rotation) as Canvas;
+        //set the health bar canvas as a child of this enemy
+        healthBarCanvas.transform.parent = transform;
+        //Get the slider component of the health bar
+        healthBar = healthBarCanvas.transform.FindChild("HealthBar").GetComponent<Slider>();
     }
 
 	
