acc2a39882763d458f10c699ac390352bbda98b4
Stanley Ung <stanleyung167@gmail.com>
2016-11-16 21:11:02 -0800

    Minimap
    Created minimap with just a blue background and the character is
    represented with a red arrow. Map is scaled to the size of the current
    map. The Minimap is a camera with just the blue background. The arrow is
    set to layer of player that I created so only the arrow is rendered
    within the minimap camera.

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour {
+
+    public Transform Target;
+
+
+    void Start() {
+        transform.position = new Vector3(Target.position.x, Target.position.y, Target.position.z);
+    }
+
+    void Update () {
+        transform.position = new Vector3(Target.position.x, Target.position.y, Target.position.z);
+        transform.rotation = new Quaternion(0, 0, Target.rotation.z, 1);
+    }
+}
