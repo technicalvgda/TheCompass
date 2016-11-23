74949c59723ddf3e78905d98f93a9da6c11d0397
j95alvarez <juanalvarez391@yahoo.com>
2016-11-22 16:10:06 -0800

    Added a ion drop
    Duster drops ions in a trail

---
+++ Assets/DropSlowIon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropSlowIon : MonoBehaviour {
+    public GameObject _ion;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        StartCoroutine(DropAndWait(5.0f));
+	}
+
+    private IEnumerator DropAndWait(float waitTime)
+    {
+        Rigidbody2D asds = Instantiate(_ion, new Vector3(gameObject.transform.position.x, transform.position.y, 0), transform.rotation) as Rigidbody2D;
+        yield return new WaitForSeconds(waitTime);
+        print("WaitAndPrint " + Time.time);
+        
+    }
+}
