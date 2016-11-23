29247f05326af6c483b7528ebc5fef77c82adec7
Darkchicken <joshshucker@hotmail.com>
2016-11-22 17:39:53 -0800

    Asteroids now spawn with random sprites
    Randomly assigned from asteroids in resources folder

---
+++ Assets/Scripts/AsteroidSpriteHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpriteHandler : MonoBehaviour {
+
+    SpriteRenderer rend;
+    Sprite[] debrisTextures;
+    int numOfTextures;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        debrisTextures = Resources.LoadAll<Sprite>("AsteroidSprites");
+        numOfTextures = debrisTextures.Length;
+        //Debug.Log("Number of tex: "+numOfTextures);   
+        rend.sprite = debrisTextures[Random.Range(0,numOfTextures-1)]; 
+	}
+	
+	
+}
