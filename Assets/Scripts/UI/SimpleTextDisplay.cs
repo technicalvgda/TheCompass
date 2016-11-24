ca743800d18d7be7bfeef48440a71169e751e63e
Ken Miller <ken.miller521@gmail.com>
2016-11-23 20:09:23 -0800

    simple text display script

---
+++ Assets/Scripts/UI/SimpleTextDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SimpleTextDisplay : MonoBehaviour {
+	private string _stringText;
+	public Text textBox;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setText(string text)
+	{
+		textBox.text = text;
+	}
+}
