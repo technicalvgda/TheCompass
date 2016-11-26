c9c0da2ef567c75ac926ab586a93b00a5a629919
fannbrian <fannbrian@gmail.com>
2016-11-26 15:29:37 -0800

    Temporary Commit
    Trying to work between my devices Lol

--- Assets/Scripts/SaveLoad/AutoSave.cs
+++ Assets/Scripts/SaveLoad/AutoSave.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class AutoSave : MonoBehaviour {
     public static string defaultFilePath = "SaveData.bin";
 	void Start () {
-        SaveLoad.SaveFile(new GameData(), defaultFilePath);	
+        GameData gd = (GameData)SaveLoad.LoadFile(defaultFilePath);
+        BranchData.Singleton = gd != null ? gd.Branch : new BranchData();
+        SaveLoad.SaveFile(new GameData(), defaultFilePath);
 	}
 }
---
+++ Assets/Scripts/TwineUnity/BranchData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization;
+using System;
+
+[Serializable]
+public class BranchData {
+    public int EnemiesKilled = 0;
+    public bool ColorVisited = false;
+    [NonSerialized]
+    public static BranchData Singleton;
+}
