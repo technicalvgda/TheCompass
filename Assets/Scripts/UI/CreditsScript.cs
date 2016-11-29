d72e049856dcc8e05cf527a724f0002cb62aee15
Ken Miller <ken.miller521@gmail.com>
2016-11-28 23:27:02 -0800

    DId some work on Credits

---
+++ Assets/Scripts/UI/CreditsScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CreditsScript : MonoBehaviour {
+	public GameObject creditsObject,mainMenuHeader;
+	public float waitTime;
+	// Use this for initialization
+	void Start () {
+		
+		StartCoroutine (StartCredits (waitTime));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	IEnumerator StartCredits(float sec)
+	{
+		yield return new WaitForSeconds (sec);
+		creditsObject.SetActive (true);
+		mainMenuHeader.SetActive (false);
+		//yield return null;
+	}
+	public void changeScene(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+}
