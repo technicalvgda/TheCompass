e1c1b549a39856d29cc6d538ef1d3fec56bc2dbc
Darkchicken <joshshucker@hotmail.com>
2016-11-30 12:33:20 -0800

    Added map ping

---
+++ Assets/Scripts/MapPing.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapPing : MonoBehaviour {
+
+    float startScale = 0.1f;
+    float endScale = 15.0f;
+    Vector3 startScaleVec, endScaleVec;
+    Vector3 scaleGrowth = new Vector3(0.1f, 0.1f, 0);
+	// Use this for initialization
+	void Start ()
+    {
+        startScaleVec = new Vector3(startScale, startScale, transform.localScale.z);
+        endScaleVec = new Vector3(endScale, endScale, transform.localScale.z);
+	}
+
+    void OnEnable()
+    {
+        transform.localScale = startScaleVec;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        transform.localScale += scaleGrowth;
+        if(transform.localScale.magnitude > endScaleVec.magnitude)
+        {
+            transform.localScale = startScaleVec;
+        }
+
+    }
+}
