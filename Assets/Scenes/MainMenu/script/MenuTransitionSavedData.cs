e66be21e19cc253e9e79add4d5c3ae24d7b59e60
Ken Miller <ken.miller521@gmail.com>
2016-12-01 14:40:19 -0800

    Redid Brian's work

--- Assets/Scenes/MainMenu/script/MenuTransitionSavedData.cs
+++ Assets/Scenes/MainMenu/script/MenuTransitionSavedData.cs
@@ -11,9 +11,9 @@ public class MenuTransitionSavedData : MenuTransition
     {
         get
         {
-            //bool hasSavedData = File.Exists(AutoSave.defaultFilePath);
+			bool hasSavedData = File.Exists(SaveLoad.defaultFilePath);
             //Debug.Log("hasSavedData = " + hasSavedData);
-            bool hasSavedData = true;
+           // bool hasSavedData = true;
 
             return !hasSavedData ? nextNew : nextContinue;
         }
